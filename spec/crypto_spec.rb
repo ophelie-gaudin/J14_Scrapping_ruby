a0a3981b4cdc450b5951add9519a40cb85a55f86
Ophélie GAUDIN <91566220+ophelie-gaudin@users.noreply.github.com>
2021-10-15 00:01:52 +0200

    Create crypto_spec.rb
    TDD for crypto.rb

---
+++ spec/crypto_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/crypto.rb'
+
+
+
+
+# describe "get_townhall_email method" do
+#  it "should return the email of the related town" do
+#    expect(rechercher_dernier_tableau(5)).to eq(5)
+    
+ # end
+end
